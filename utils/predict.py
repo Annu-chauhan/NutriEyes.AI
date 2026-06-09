3e3715fc5dd1a09d49681897516de09899b8a489
Parul <chauhanparul0001@gmail.com>
2026-06-09 20:35:30 +0530

    Fix model path

--- utils/predict.py
+++ utils/predict.py
@@ -16,12 +16,7 @@ from utils.gradcam import (
 
 print("Retina model loading...")
 
-
-
-MODEL_PATH = os.path.join(
-    "model",
-    "retinal_5class.h5"
-)
+MODEL_PATH = "retinal_5class.h5"
 
 print("Using local model:", MODEL_PATH)
 
